9917a91dbfe63d0441069eda91127608a4bd70af
jur <jelle@samson-it.nl>
2015-06-08 12:24:48 +0200

    dont fill 0 in timeboxes.

--- go.js
+++ go.js
@@ -64,6 +64,7 @@ if(lastOpened !== null) {
     var diff = new Date().getTime() - parseInt(lastOpened);
     var inputs = $('.arbeidDuur');
     var lastOne = inputs[inputs.length -1];
+    if(diff == 0) return;
     lastOne.value = new Number(diff / 60 / 60 / 60 / 60).toFixed(2);
 }
 
